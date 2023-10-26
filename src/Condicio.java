6ec05eebed6d99c968d53297fe783472b20df270
guilhermemoraessiqueira <guimoraessiqueira26@gmail.com>
2023-10-26 12:11:04 -0300

    feat: Primeiro programa em Java e Construindo o catálogo de filmes

---
+++ src/Condicio.java
@@ -0,0 +1,2 @@
+public class Condicio {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: "+anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double nota = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom"
+                Ana de lançamento
+                """ + anoDeLancamento;
+
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media/2);
+        System.out.println(classificacao);
+
     }
 }
